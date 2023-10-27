c888e4ab673c6a7fa513b883decb08ebff95f81b
SHIV KUMAR <shivk7466@gmail.com>
2023-10-27 21:50:56 +0530

    perf: remove graphiteWidth option testing

--- src/math.ts
+++ src/math.ts
@@ -5,5 +5,10 @@ export function add(x: number, y: number): number {
 export function multiply(x: number, y: number): number {
   console.log(x);
   console.log(y);
+  console.log("kjhjhkl")
   return x * y;
 }
+// perf(pencil): remove graphiteWidth option
+
+// BREAKING CHANGE: The graphiteWidth option has been removed.
+// The default graphite width of 10mm is always used for performance reasons.
